96ee4156be93330890d9a1d8bfb07b86690bd19b
karlosvas <126987511+karlosvas@users.noreply.github.com>
2023-08-06 19:37:09 +0200

    V.2 Update all

---
+++ objetos-process/7.process.js
@@ -0,0 +1,17 @@
+//Process, argumentos de entrada, crea un array con la ruta, y los argumentos que le demos.
+console.log(process.argv)
+
+//Podemos controlar eventos del proceso
+process.on('exit', ()=>{
+    //limpiar los recursos
+})
+
+//Current woirking directory, sitio desde el que se ejecuta el proceso.
+console.log(process.cwd())
+
+//Platform, deveremos asignarle un valor en el proceso a VALOR.
+console.log(process.env.VALOR)
+
+/*contrrolar el proceso, con 0 es que todo a ido bien y el proceso tiene que terminar,
+con 1 es que ha habido un error y queremos que termine porque puede perjudicar al código*/
+process.exit(1)
---
+++ path/path.js
@@ -0,0 +1,17 @@
+const path = require('node:path')
+//Unir rutas con path.join
+console.log(path.sep)
+//Esto indica la barra separadora de carpetas según el sistema operativo.
+const filePath = path.join('content','subfolder','text.txt')
+console.log(filePath)
+//Para obtener el nombre del fichero de una ruta utilizamos base()
+const base = path.basename(filePath)
+const base2 = path.basename('Documents/learn-nodejs/path/path.js')
+console.log(base)
+console.log(base2)
+//Para obtener el nombre del fichero y cquitar la extensión agregamos una coma.
+const imaginePaswords = path.basename('/tmp/prank-secret/password.txt', '.txt')
+console.log(imaginePaswords)
+//Saber la extension del fichero buscado.
+const extension = path.extname('image.png')
+console.log(extension)
