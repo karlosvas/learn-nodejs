7d906faa07673003a0960386e26c8457a47ea8c3
karlosvas <126987511+karlosvas@users.noreply.github.com>
2023-08-08 01:02:33 +0200

    v.4 Uppdate finish second class

---
+++ clase2/2.routing.js
@@ -0,0 +1,42 @@
+const http = require('node:http')
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+          return res.end('404')
+      }
+    case 'POST':
+      switch (url) {
+        case '/pokemon': {
+          let body = ''
+          // Escuchar el evento data
+          req.on('data', chunk => {
+            body += chunk
+          })
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // guardar info en base de datos
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data))
+          })
+        }
+          break
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log('server listening on port http://localhost:1234')
+})
