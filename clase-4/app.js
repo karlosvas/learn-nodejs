6d183e02b708280025c747c61e5ae26e2652a38b
karlosvas <126987511+karlosvas@users.noreply.github.com>
2023-09-13 01:37:37 +0200

    Eliminated submodule

---
+++ clase-4/app.js
@@ -0,0 +1,38 @@
+import express, { json } from 'express';
+import { moviesRouter } from './routes/movies.js';
+import { corsMidelware } from './midelwares/cors.js';
+import { readJSON } from './utils.js';
+
+// EN EL FUTURO EL IMPORT SERA ASI:
+//import movies from './movies.json' whith { type: 'json' }
+
+/* Como leer un json en ESModules
+import fs from  'node:fs'
+const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+*/
+
+// Como leer un json en ESModules recomendado por ahora
+// import { createRequire } from 'node:module';
+// const require = createRequire(import.meta.url);
+// const movies = require('./movies.json')
+
+const movies = readJSON('./movies.json')
+
+const app = express()
+app.use(json())
+app.use(corsMidelware())
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, "localhost", () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
